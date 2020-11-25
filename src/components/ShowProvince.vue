3e549cce4fb2e48a90c7940c334e63ebb50be148
Mangkorn7718 <katawong0@gmail.com>
2020-11-25 15:12:58 +0700

    update Probince page

--- src/components/ShowProvince.vue
+++ src/components/ShowProvince.vue
@@ -62,9 +62,11 @@
 
 <script>
 import firebase from 'firebase/app'
+import {db} from "@/firebase_config";
 export default {
     data () {
       return {
+        objectArray: [],
         itemsPerPageArray: [4, 8, 12],
         search: '',
         filter: {},
@@ -74,116 +76,11 @@ export default {
         sortBy: 'name',
         keys: [
           'Name',
-          'Calories',
-          'Fat',
-          'Carbs',
-          'Protein',
-          'Sodium',
-          'Calcium',
-          'Iron',
+          'Today',
+          'Yesterday',
+          'Total',
         ],
-        items: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            sodium: 87,
-            calcium: '14%',
-            iron: '1%',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            sodium: 129,
-            calcium: '8%',
-            iron: '1%',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            sodium: 337,
-            calcium: '6%',
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            sodium: 413,
-            calcium: '3%',
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            sodium: 327,
-            calcium: '7%',
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            sodium: 50,
-            calcium: '0%',
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            sodium: 38,
-            calcium: '0%',
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            sodium: 562,
-            calcium: '0%',
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            sodium: 326,
-            calcium: '2%',
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            sodium: 54,
-            calcium: '12%',
-            iron: '6%',
-          },
-        ],
+        items: [],
       }
     },
     computed: {
@@ -197,10 +94,19 @@ export default {
   created() {
       firebase.auth().currentUser.getIdToken().then(
           token => {
-            console.log(token)
             axios.get('https://anonymoussocial-cfece.firebaseio.com/Posts.json/?auth='+token).then(
                 response => {
-                  console.log(response.data)
+                  for(let key in response.data){
+                    let obj = {}
+                    this.objectArray.push(key)
+                    db.ref('/Posts/'+key).once('value',(data) => {
+                      obj["name"] = key
+                      obj["today"] = data.numChildren().toString()
+                      obj["total"] = data.numChildren().toString()
+                      this.items.push(obj)
+                      console.log(this.items)
+                    })
+                  }
                 }
             )
           }
